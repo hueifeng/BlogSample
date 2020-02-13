4a7f9103e9c1a5f9aa1a0b8ab333192f8e2ce224
HueiFeng <695979933@qq.com>
2020-02-13 14:29:04 +0800

    Magicodes.IE

---
+++ src/Magicodes.IE.Exporter/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Magicodes.IE.Exporter.Models;
+using Magicodes.ExporterAndImporter.Excel;
+using Magicodes.ExporterAndImporter.Core;
+using System.IO;
+using Magicodes.ExporterAndImporter.Pdf;
+using System.Data;
+
+namespace Magicodes.IE.Exporter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ExporterExcel() {
+            IExporter exporter = new ExcelExporter();
+           
+            var result = await exporter.Export(Path.Combine("wwwroot","test.xlsx"), new List<StudentExcel>()
+                {
+                    new StudentExcel
+                    {
+                        Name = "MR.A",
+                        Age = 18,
+                        Remarks = "我叫MR.A,今年18岁",
+                        Birthday=DateTime.Now
+                    },
+                    new StudentExcel
+                    {
+                        Name = "MR.B",
+                        Age = 19,
+                        Remarks = "我叫MR.B,今年19岁",
+                        Birthday=DateTime.Now
+                    },
+                    new StudentExcel
+                    {
+                        Name = "MR.C",
+                        Age = 20,
+                        Remarks = "我叫MR.C,今年20岁",
+                        Birthday=DateTime.Now
+                    }
+                });
+            return File("test.xlsx", "application/ms-excel", result.FileName);
+        }
+
+        public async Task<IActionResult> ExporterPdf() {
+            var exporter = new PdfExporter();
+            var result = await exporter.ExportListByTemplate(Path.Combine("wwwroot", "test.pdf"), new List<StudentPdf>()
+            {
+                 new StudentPdf
+                    {
+                        Name = "MR.A",
+                        Age = 18,
+                        Remarks = "我叫MR.A,今年18岁",
+                        Birthday=DateTime.Now
+                    },
+                    new StudentPdf
+                    {
+                        Name = "MR.B",
+                        Age = 19,
+                        Remarks = "我叫MR.B,今年19岁",
+                        Birthday=DateTime.Now
+                    },
+                    new StudentPdf
+                    {
+                        Name = "MR.C",
+                        Age = 20,
+                        Remarks = "我叫MR.C,今年20岁",
+                        Birthday=DateTime.Now
+                    }
+            });
+            return File("test.pdf", "application/pdf", result.FileName);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
