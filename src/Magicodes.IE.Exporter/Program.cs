952c8ba5ba842c87e4e31459d03f13baf068d168
HueiFeng <695979933@qq.com>
2020-02-14 17:16:52 +0800

    Update

--- src/Magicodes.IE.Exporter/Program.cs
+++ src/Magicodes.IE.Exporter/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Magicodes.IE.Exporter
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -24,3 +24,4 @@ namespace Magicodes.IE.Exporter
                 });
     }
 }
+
