161a64c2a18cd452247b185354d8165d69dc7f48
辉 冯 <695979933@qq.com>
2020-02-23 14:00:03 +0800

    添加ResponseCachingDemo

---
+++ src/ResponseCachingDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResponseCachingDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
