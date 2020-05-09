4c1c4894bb9ec34648559224be92bccf3cfa1356
HueiFeng <695979933@qq.com>
2020-05-09 15:16:32 +0800

    Update MagicodesFilter

--- src/Magicodes.64/Filters/MagicodesFilter.cs
+++ src/Magicodes.64/Filters/MagicodesFilter.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System.Threading.Tasks;
+using Magicodes._64.Attributes;
+using Microsoft.AspNetCore.Http;
 
 namespace Magicodes._64.Filters
 {
@@ -10,11 +12,23 @@ namespace Magicodes._64.Filters
     {
         public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
         {
-            var obj = (ObjectResult)context.Result;
-            var timeConverter = new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd HH:mm:ss" };
-            var json = JsonConvert.SerializeObject(obj.Value, timeConverter);
-            var type = obj.Value.GetType().GenericTypeArguments[0];
-            await new Extensions().HandleSuccessfulReqeustAsync(context.HttpContext, json, 200, type);
+            var result = context.Result;
+            if (result is ObjectResult objectResult)
+            {
+                var endpoint = context.HttpContext.GetEndpoint();
+                var endpointMagicodesData = endpoint?.Metadata.GetMetadata<IMagicodesData>();
+                if (endpointMagicodesData != null)
+                {
+                    var timeConverter = new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd HH:mm:ss" };
+                    var json = JsonConvert.SerializeObject(objectResult.Value, timeConverter);
+                    await new Extensions().HandleSuccessfulReqeustAsync(context: context.HttpContext, body: json, httpStatusCode: 200,
+                        type: endpointMagicodesData.Type);
+                }
+            }
+            else
+            {
+                await next();
+            }
         }
 
     }
