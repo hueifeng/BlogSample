9b0b9f544a40b9d77da70bd05b73d483ab6fd0b8
HueiFeng <695979933@qq.com>
2020-05-09 18:38:26 +0800

    Update Magicodes.64

--- src/Magicodes.64/Attributes/IMagicodesData.cs
+++ src/Magicodes.64/Attributes/IMagicodesData.cs
@@ -4,6 +4,13 @@ namespace Magicodes._64.Attributes
 {
     public interface IMagicodesData
     {
+        /// <summary>
+        ///   Model Type  
+        /// </summary>
         Type Type { get; set; }
+        /// <summary>
+        ///     Template Path
+        /// </summary>
+        string TemplatePath { get; set; }
     }
 }
--- src/Magicodes.64/Attributes/MagicodesAttribute.cs
+++ src/Magicodes.64/Attributes/MagicodesAttribute.cs
@@ -5,6 +5,13 @@ namespace Magicodes._64.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class MagicodesAttribute : Attribute, IMagicodesData
     {
+        /// <summary>
+        ///     Model Type
+        /// </summary>
         public Type Type { get; set; }
+        /// <summary>
+        ///     Template Path
+        /// </summary>
+        public string TemplatePath { get; set; }
     }
 }
