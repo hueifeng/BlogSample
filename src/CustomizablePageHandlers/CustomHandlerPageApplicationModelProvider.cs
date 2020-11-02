3affa0b4183961d5d28086a46988cd3cd98ba59a
Huei Feng <695979933@qq.com>
2020-11-02 23:22:39 +0800

    Added CustomizablePageHandlers

---
+++ src/CustomizablePageHandlers/CustomHandlerPageApplicationModelProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace CustomizablePageHandlers
+{
+    public class CustomHandlerPageApplicationModelProvider : IPageApplicationModelProvider
+    {
+        public void OnProvidersExecuting(PageApplicationModelProviderContext context)
+        {
+            var a = context;
+        }
+
+        public void OnProvidersExecuted(PageApplicationModelProviderContext context)
+        {
+            var a = context;
+        }
+
+        // The order is set to execute after the DefaultPageApplicationModelProvider.
+        public int Order => -1000 + 10;
+    }
+}
