ea9d6be4524611f6a6d026abfced57b82e879d2e
Huei Feng <695979933@qq.com>
2021-02-10 20:43:25 +0800

    Add DebuggerDisplayAttributes

---
+++ src/DebuggerDisplayAttributes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DebuggerDisplayAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            MyClass myClass = new MyClass();
+            Console.WriteLine("Hello World!");
+        }
+
+        /**
+         *DebuggerTypeProxy用于代理显示某个类，不会显示私有成员，只显示公共成员
+         */
+
+        [DebuggerTypeProxy(typeof(Student))]
+        class MyClass
+        {
+            
+        }
+
+        [DebuggerDisplay("Name={Name},Age={Age}")]
+        class Student
+        {
+            public Student()
+            {
+                Students = new List<MyClass>
+                {
+                    new MyClass(),
+                    new MyClass(),
+                    new MyClass()
+                };
+            }
+
+            /**
+             *DebuggerBrowsableState
+             *Never可隐藏字段属性
+             *Collapsed默认选项，显示成员信息
+             *RootHidden 不显示字段，如果是数组或者集合将以成对的对象形式显示
+             */
+
+            [DebuggerBrowsable(DebuggerBrowsableState.Collapsed)]
+            public static string y = "Test String";
+
+            [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+            public int Age { get; set; }
+
+            [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+            public string Name { get; set; }
+
+            [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+            public List<MyClass> Students { get; set; }
+        }
+
+    }
+}
