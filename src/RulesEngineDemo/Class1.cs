4be164feb3c78114cce70792242c333f114f5dc0
Huei Feng <695979933@qq.com>
2021-04-29 23:12:07 +0800

    Update

--- src/RulesEngineDemo/Class1.cs
+++ src/RulesEngineDemo/Class1.cs
@@ -1,126 +1,141 @@
-﻿//using Newtonsoft.Json;
-//using RulesEngine.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using RulesEngine.Extensions;
+using RulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace RulesEngineDemo
-//{
-//    class Program
-//    {
-//        private static readonly ReSettings reSettings = new ReSettings
-//        {
-//            CustomTypes = new[] { typeof(IdCardUtil) }
-//        };
+namespace RulesEngineDemo
+{
+    class Program
+    {
+        private static readonly ReSettings reSettings = new ReSettings
+        {
+            CustomTypes = new[] { typeof(IdCardUtil) }
+        };
 
-//        static async Task Main(string[] args)
-//        {
-//            //定义规则
-//            var rulesStr = @"[{
-//                    ""WorkflowName"": ""UserInputWorkflow"",
-//                    ""Rules"": [
-//                      {
-//                        ""RuleName"": ""CheckNestedSimpleProp"",
-//                        ""ErrorMessage"": ""年龄必须小于18岁."",
-//                        ""ErrorType"": ""Error"",
-//                        ""RuleExpressionType"": ""LambdaExpression"",
-//                        ""Expression"": ""IdNo.GetAgeByIdCard() < 18""
-//                      },
-//                       {
-//                        ""RuleName"": ""CheckNestedSimpleProp1"",
-//                        ""ErrorMessage"": ""身份证号不可以为空."",
-//                         ""ErrorType"": ""Error"",
-//                        ""RuleExpressionType"": ""LambdaExpression"",
-//                        ""Expression"": ""IdNo != null""
-//                      }
-//                    ]
-//                  }] ";
+        static async Task Main(string[] args)
+        {
+            //定义规则
+            var rulesStr = @"[{
+                    ""WorkflowName"": ""UserInputWorkflow"",
+                    ""Rules"": [
+                      {
+                        ""RuleName"": ""CheckNestedSimpleProp"",
+                        ""ErrorMessage"": ""年龄必须小于18岁."",
+                        ""ErrorType"": ""Error"",
+                        ""RuleExpressionType"": ""LambdaExpression"",
+                        ""Expression"": ""IdNo.GetAgeByIdCard() < 18""
+                      },
+                       {
+                        ""RuleName"": ""CheckNestedSimpleProp1"",
+                        ""ErrorMessage"": ""身份证号不可以为空."",
+                         ""ErrorType"": ""Error"",
+                        ""RuleExpressionType"": ""LambdaExpression"",
+                        ""Expression"": ""IdNo != null""
+                      }
+                    ]
+                  }] ";
 
-//            //规则JSON架构
-//            //RuleName规则名称
-//            //Properties 规则属性，获取或设置规则的自定义属性或标记。
-//            //Operator 操作符
-//            //ErrorMessage 错误消息
-//            //Enabled 获取和设置规则是否已启用
-//            //RuleExpressionType 默认RuleExpressionType.LambdaExpression
-//            //WorkflowRulesToInject 注入工作规则
-//            //Rules 规则
-//            //LocalParams 
-//            //Expression 表达树
-//            //Actions 
-//            //SuccessEvent 完成事件，默认为规则名称
-
-
-
-//            var userInput = new UserInput
-//            {
-//                IdNo = "11010519491230002X",
-//                Age = 18
-//            };
-
-//            //反序列化Json格式规则字符串
-//            var workflowRules = JsonConvert.DeserializeObject<List<WorkflowRules>>(rulesStr);
+            //规则JSON架构
+            //RuleName规则名称
+            //Properties 规则属性，获取或设置规则的自定义属性或标记。
+            //Operator 操作符
+            //ErrorMessage 错误消息
+            //Enabled 获取和设置规则是否已启用
+            //RuleExpressionType 默认RuleExpressionType.LambdaExpression
+            //WorkflowRulesToInject 注入工作规则
+            //Rules 规则
+            //LocalParams 
+            //Expression 表达树
+            //Actions 
+            //SuccessEvent 完成事件，默认为规则名称
 
 
 
-//            var rulesEngine = new RulesEngine.RulesEngine(workflowRules.ToArray(), null, reSettings: reSettings);
+            var userInput = new UserInput
+            {
+                IdNo = "11010519491230002X",
+                Age = 18
+            };
 
-//            List<RuleResultTree> resultList = await rulesEngine.ExecuteAllRulesAsync("UserInputWorkflow", userInput);
-//            foreach (var item in resultList)
-//            {
-//                //{ "Rule":{ "RuleName":"CheckNestedSimpleProp","Properties":null,"Operator":null,"ErrorMessage":"年龄必须大于18岁.",
-//                //"ErrorType":"Error","RuleExpressionType":"LambdaExpression","WorkflowRulesToInject":null,"Rules":null,"LocalParams":null,"Expression":"Age > 18","Actions":null,"SuccessEvent":null},"IsSuccess":false,"ChildResults":null,"Inputs":{ "input1":{ "IdNo":null,"Age":18} },
-//                //"ActionResult":{ "Output":null,"Exception":null},"ExceptionMessage":"年龄必须大于18岁.","RuleEvaluatedParams":[]}
-
-
-//                Console.WriteLine("验证成功：{0}，消息：{1}", item.IsSuccess, item.ExceptionMessage);
-//            }
-
-//            Console.ReadLine();
-
-//        }
+            //反序列化Json格式规则字符串
+            var workflowRules = JsonConvert.DeserializeObject<List<WorkflowRules>>(rulesStr);
 
 
-//        public class UserInput
-//        {
-//            public int UserId { get; set; }
-//            public string IdNo { get; set; }
-//            public int Age { get; set; }
-//        }
 
-//        public class Goods
-//        {
-//            public int Id { get; set; }
+            var rulesEngine = new RulesEngine.RulesEngine(workflowRules.ToArray(), null, reSettings: reSettings);
 
-//            public int Name { get; set; }
+            List<RuleResultTree> resultList = await rulesEngine.ExecuteAllRulesAsync("UserInputWorkflow", userInput);
+            //foreach (var item in resultList)
+            //{
+            //    //{ "Rule":{ "RuleName":"CheckNestedSimpleProp","Properties":null,"Operator":null,"ErrorMessage":"年龄必须大于18岁.",
+            //    //"ErrorType":"Error","RuleExpressionType":"LambdaExpression","WorkflowRulesToInject":null,"Rules":null,"LocalParams":null,"Expression":"Age > 18","Actions":null,"SuccessEvent":null},"IsSuccess":false,"ChildResults":null,"Inputs":{ "input1":{ "IdNo":null,"Age":18} },
+            //    //"ActionResult":{ "Output":null,"Exception":null},"ExceptionMessage":"年龄必须大于18岁.","RuleEvaluatedParams":[]}
 
-//            public int UserId { get; set; }
-//        }
-//    }
 
-//    public static class IdCardUtil
-//    {
-//        public static int GetAgeByIdCard(this string idCard)
-//        {
-//            int age = 0;
-//            if (!string.IsNullOrWhiteSpace(idCard))
-//            {
-//                var subStr = string.Empty;
-//                if (idCard.Length == 18)
-//                {
-//                    subStr = idCard.Substring(6, 8).Insert(4, "-").Insert(7, "-");
-//                }
-//                else if (idCard.Length == 15)
-//                {
-//                    subStr = ("19" + idCard.Substring(6, 6)).Insert(4, "-").Insert(7, "-");
-//                }
-//                TimeSpan ts = DateTime.Now.Subtract(Convert.ToDateTime(subStr));
-//                age = ts.Days / 365;
-//            }
-//            return age;
-//        }
+            //    Console.WriteLine("验证成功：{0}，消息：{1}", item.IsSuccess, item.ExceptionMessage);
+            //}
 
-//    }
-//}
+            var discountOffered = "";
+
+            resultList.OnSuccess((eventName) =>
+            {
+                discountOffered = $"成功事件:{eventName}.";
+            });
+
+
+            resultList.OnFail(() =>
+            {
+                discountOffered = "失败事件.";
+            });
+
+            Console.WriteLine(discountOffered);
+            Console.ReadLine();
+
+        }
+
+
+        public class UserInput
+        {
+            public int UserId { get; set; }
+            public string IdNo { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class Goods
+        {
+            public int Id { get; set; }
+
+            public int Name { get; set; }
+
+            public int UserId { get; set; }
+        }
+    }
+
+    public static class IdCardUtil
+    {
+        public static int GetAgeByIdCard(this string idCard)
+        {
+            int age = 0;
+            if (!string.IsNullOrWhiteSpace(idCard))
+            {
+                var subStr = string.Empty;
+                if (idCard.Length == 18)
+                {
+                    subStr = idCard.Substring(6, 8).Insert(4, "-").Insert(7, "-");
+                }
+                else if (idCard.Length == 15)
+                {
+                    subStr = ("19" + idCard.Substring(6, 6)).Insert(4, "-").Insert(7, "-");
+                }
+                TimeSpan ts = DateTime.Now.Subtract(Convert.ToDateTime(subStr));
+                age = ts.Days / 365;
+            }
+            return age;
+        }
+
+    }
+}
--- src/RulesEngineDemo/Program.cs
+++ src/RulesEngineDemo/Program.cs
@@ -1,148 +1,148 @@
-﻿using Newtonsoft.Json;
-using RulesEngine.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Dynamic.Core;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
+﻿//using Newtonsoft.Json;
+//using RulesEngine.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Dynamic.Core;
+//using System.Linq.Expressions;
+//using System.Threading.Tasks;
 
-namespace RulesEngineDemo
-{
-    class Program
-    {
-        private static readonly ReSettings reSettings = new ReSettings
-        {
-            CustomTypes = new[] { typeof(IdCardUtil) }
-        };
+//namespace RulesEngineDemo
+//{
+//    class Program
+//    {
+//        private static readonly ReSettings reSettings = new ReSettings
+//        {
+//            CustomTypes = new[] { typeof(IdCardUtil) }
+//        };
 
-        static async Task Main(string[] args)
-        {
-            //定义规则
-            var rulesStr = @"[{
-                    ""WorkflowName"": ""UserInputWorkflow"",
-                    ""Rules"": [
-                      {
-                        ""RuleName"": ""CheckNestedSimpleProp"",
-                        ""ErrorMessage"": ""Value值不是second."",
-                        ""ErrorType"": ""Error"",
-                        ""RuleExpressionType"": ""LambdaExpression"",
-                        ""Expression"": ""user.UserId==1 && items[0].Value==second""
-                      }
-                    ]
-                  }] ";
-
-
-            var userInput = new UserInput
-            {
-                UserId = 1,
-                IdNo = "11010519491230002X",
-                Age = 18
-            };
-
-            var input = new
-            {
-                user = userInput,
-                items = new List<ListItem>()
-                {
-                    new ListItem{ Id=1,Value="first"},
-                    new ListItem{ Id=2,Value="second"}
-                }
-            };
+//        static async Task Main(string[] args)
+//        {
+//            //定义规则
+//            var rulesStr = @"[{
+//                    ""WorkflowName"": ""UserInputWorkflow"",
+//                    ""Rules"": [
+//                      {
+//                        ""RuleName"": ""CheckNestedSimpleProp"",
+//                        ""ErrorMessage"": ""Value值不是second."",
+//                        ""ErrorType"": ""Error"",
+//                        ""RuleExpressionType"": ""LambdaExpression"",
+//                        ""Expression"": ""user.UserId==1 && items[0].Value==second""
+//                      }
+//                    ]
+//                  }] ";
 
 
-            //反序列化Json格式规则字符串
-            var workflowRules = JsonConvert.DeserializeObject<List<WorkflowRules>>(rulesStr);
-            var rulesEngine = new RulesEngine.RulesEngine(workflowRules.ToArray(), null, reSettings: reSettings);
+//            var userInput = new UserInput
+//            {
+//                UserId = 1,
+//                IdNo = "11010519491230002X",
+//                Age = 18
+//            };
 
-            List<RuleResultTree> resultList = await rulesEngine.ExecuteAllRulesAsync("UserInputWorkflow", input);
-            foreach (var item in resultList)
-            {
-                //Console.WriteLine("验证成功：{0}，消息：{1}", item.IsSuccess, item.ExceptionMessage);
-            }
-
-
-            Expression<Func<ListItem, bool>> predicate = x => x.Id == 1;
-            //输入条件如下
-            var inputItem = new ListItem
-            {
-                Id = 1,
-                Value = "second"
-            };
-
-            if (inputItem.Id !=null)
-            {
-                predicate = predicate.And(x=>x.Id==inputItem.Id);
-            }
-
-            if (inputItem.Id != null)
-            {
-                predicate = predicate.And(x => x.Value == inputItem.Value);
-            }
+//            var input = new
+//            {
+//                user = userInput,
+//                items = new List<ListItem>()
+//                {
+//                    new ListItem{ Id=1,Value="first"},
+//                    new ListItem{ Id=2,Value="second"}
+//                }
+//            };
 
 
-            var items = input.items.AsQueryable().Where("Id ==@0  && Value==@1",inputItem.Id,inputItem.Value).ToList();
+//            //反序列化Json格式规则字符串
+//            var workflowRules = JsonConvert.DeserializeObject<List<WorkflowRules>>(rulesStr);
+//            var rulesEngine = new RulesEngine.RulesEngine(workflowRules.ToArray(), null, reSettings: reSettings);
+
+//            List<RuleResultTree> resultList = await rulesEngine.ExecuteAllRulesAsync("UserInputWorkflow", input);
+//            foreach (var item in resultList)
+//            {
+//                Console.WriteLine("验证成功：{0}，消息：{1}", item.IsSuccess, item.ExceptionMessage);
+//            }
+
+
+//            //Expression<Func<ListItem, bool>> predicate = x => x.Id == 1;
+//            ////输入条件如下
+//            //var inputItem = new ListItem
+//            //{
+//            //    Id = 1,
+//            //    Value = "second"
+//            //};
+
+//            //if (inputItem.Id !=null)
+//            //{
+//            //    predicate = predicate.And(x=>x.Id==inputItem.Id);
+//            //}
+
+//            //if (inputItem.Id != null)
+//            //{
+//            //    predicate = predicate.And(x => x.Value == inputItem.Value);
+//            //}
+
+
+//            //var items = input.items.AsQueryable().Where("Id ==@0  && Value==@1",inputItem.Id,inputItem.Value).ToList();
 
 
 
-            foreach (var item in items)
-            {
-                Console.WriteLine($"Id：{item.Id},Value: {item.Value}");
-            }
+//            //foreach (var item in items)
+//            //{
+//            //    Console.WriteLine($"Id：{item.Id},Value: {item.Value}");
+//            //}
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
-        }
-
-
-        public class UserInput
-        {
-            public int UserId { get; set; }
-            public string IdNo { get; set; }
-            public int Age { get; set; }
-        }
-
-        public class ListItem
-        {
-            public int? Id { get; set; }
-
-            public string Value { get; set; }
-        }
-    }
+//        }
 
 
-    public static class PredicateBuilder
-    {
-        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
-                                                            Expression<Func<T, bool>> expr2)
-        {
-            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
-            return Expression.Lambda<Func<T, bool>>
-                  (Expression.And(expr1.Body, invokedExpr), expr1.Parameters);
-        }
-    }
+//        public class UserInput
+//        {
+//            public int UserId { get; set; }
+//            public string IdNo { get; set; }
+//            public int Age { get; set; }
+//        }
 
-    public static class IdCardUtil
-    {
-        public static int GetAgeByIdCard(this string idCard)
-        {
-            int age = 0;
-            if (!string.IsNullOrWhiteSpace(idCard))
-            {
-                var subStr = string.Empty;
-                if (idCard.Length == 18)
-                {
-                    subStr = idCard.Substring(6, 8).Insert(4, "-").Insert(7, "-");
-                }
-                else if (idCard.Length == 15)
-                {
-                    subStr = ("19" + idCard.Substring(6, 6)).Insert(4, "-").Insert(7, "-");
-                }
-                TimeSpan ts = DateTime.Now.Subtract(Convert.ToDateTime(subStr));
-                age = ts.Days / 365;
-            }
-            return age;
-        }
+//        public class ListItem
+//        {
+//            public int? Id { get; set; }
 
-    }
-}
+//            public string Value { get; set; }
+//        }
+//    }
+
+
+//    public static class PredicateBuilder
+//    {
+//        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
+//                                                            Expression<Func<T, bool>> expr2)
+//        {
+//            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+//            return Expression.Lambda<Func<T, bool>>
+//                  (Expression.And(expr1.Body, invokedExpr), expr1.Parameters);
+//        }
+//    }
+
+//    public static class IdCardUtil
+//    {
+//        public static int GetAgeByIdCard(this string idCard)
+//        {
+//            int age = 0;
+//            if (!string.IsNullOrWhiteSpace(idCard))
+//            {
+//                var subStr = string.Empty;
+//                if (idCard.Length == 18)
+//                {
+//                    subStr = idCard.Substring(6, 8).Insert(4, "-").Insert(7, "-");
+//                }
+//                else if (idCard.Length == 15)
+//                {
+//                    subStr = ("19" + idCard.Substring(6, 6)).Insert(4, "-").Insert(7, "-");
+//                }
+//                TimeSpan ts = DateTime.Now.Subtract(Convert.ToDateTime(subStr));
+//                age = ts.Days / 365;
+//            }
+//            return age;
+//        }
+
+//    }
+//}
