747695305d61f5338f8d8c09ed0085d3c81a8e90
hueifeng <695979933@qq.com>
2023-08-13 21:20:24 +0800

    add ObjectLayout

---
+++ ObjectLayout/Program.cs
@@ -0,0 +1,14 @@
+﻿using ObjectLayout;
+using ObjectLayoutInspector;
+
+TypeLayout.PrintLayout<SampleStruct>();
+Console.Read();
+
+[System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Auto)]
+public struct SampleStruct
+{
+    public byte X;
+    public double Y;
+    public int Z;
+}
+
---
+++ ObjectLayout/SampleStruct.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectLayout
+{
+    public struct SampleStruct
+    {
+        public int Value1; 
+        public int Value2;
+        public int Value3;
+        public int Value4;
+    }
+}
